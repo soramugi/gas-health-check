e432eafb034b97f3df07b90b9059640449d01a8c
soramugi <soramugi.chika@gmail.com>
2018-06-11 08:05:37 +0900

    死活監視できるように

--- src/Code.ts
+++ src/Code.ts
@@ -1,14 +1,42 @@
-function myFunction() {
-  Logger.log("Hello World")
-}
-
 function setScheduling() {
   const triggers = ScriptApp.getProjectTriggers();
-  triggers.forEach((t) => {
-    ScriptApp.deleteTrigger(t);
-  })
-  ScriptApp.newTrigger('myFunction')
+  triggers.forEach(t => ScriptApp.deleteTrigger(t))
+  ScriptApp.newTrigger('execution')
     .timeBased()
     .everyMinutes(1)
     .create();
+}
+
+
+const to = "soramugi.chika@gmail.com";
+const urls = [
+  "https://www.google.com",
+  "http://example.com",
+  "http://nothing.example.com",
+]
+
+function execution() { urls.forEach(fetchUrl) }
+
+const fetchUrl = (url: string) => {
+  let statusCode: number = 0;
+  try {
+    const response = UrlFetchApp.fetch(url, {muteHttpExceptions: true});
+    statusCode = response.getResponseCode();
+  } catch(err) {
+    statusCode = 999;
+  }
+
+  const statusPreCode =  PropertiesService.getScriptProperties().getProperty(url);
+  if (statusCode !== parseInt(statusPreCode)) {
+    PropertiesService.getScriptProperties().setProperty(url, String(statusCode));
+    sendMail(url, statusCode);
+  }
+}
+
+const sendMail = (url: string, statusCode: number) => {
+  const state = (statusCode === 200) ? "OK" : "ALARM";
+  const subject = "[gas-health-check] [" + state + "] " + statusCode + ": " + url ;
+  const body = state + "\n" + statusCode + "\n" + url;
+
+  MailApp.sendEmail(to, subject, body);
 }

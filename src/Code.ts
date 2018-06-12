339a325e554de8be3eee80a6a24832c8763bb5b4
soramugi <soramugi.chika@gmail.com>
2018-06-13 08:08:04 +0900

    スクリプトプロパティから監視URL,メールアドレスを取得するように

--- src/Code.ts
+++ src/Code.ts
@@ -1,23 +1,25 @@
+
 function setScheduling() {
   const triggers = ScriptApp.getProjectTriggers();
   triggers.forEach(t => ScriptApp.deleteTrigger(t))
-  ScriptApp.newTrigger('execution')
+  ScriptApp.newTrigger("execution")
     .timeBased()
     .everyMinutes(1)
     .create();
 }
 
+const scriptProperties = PropertiesService.getScriptProperties()
 
-const to = "soramugi.chika@gmail.com";
-const urls = [
-  "https://www.google.com",
-  "http://example.com",
-  "http://nothing.example.com",
-]
+function execution() {
+  const properties: any = scriptProperties.getProperties()
+  for (let key in properties) {
+    if (/^http/.test(key)) {
+      fetchUrl(key, properties[key])
+    }
+  }
+}
 
-function execution() { urls.forEach(fetchUrl) }
-
-const fetchUrl = (url: string) => {
+const fetchUrl = (url: string, statusPreCode: string) => {
   let statusCode: number = 0;
   try {
     const response = UrlFetchApp.fetch(url, {muteHttpExceptions: true});
@@ -26,15 +28,15 @@ const fetchUrl = (url: string) => {
     statusCode = 999;
   }
 
-  const statusPreCode =  PropertiesService.getScriptProperties().getProperty(url);
   if (statusCode !== parseInt(statusPreCode)) {
-    PropertiesService.getScriptProperties().setProperty(url, String(statusCode));
+    scriptProperties.setProperty(url, String(statusCode));
     sendMail(url, statusCode);
   }
 }
 
 const sendMail = (url: string, statusCode: number) => {
   const state = (statusCode === 200) ? "OK" : "ALARM";
+  const to = scriptProperties.getProperty("to")
   const subject = "[gas-health-check] [" + state + "] " + statusCode + ": " + url ;
   const body = state + "\n" + statusCode + "\n" + url;
 
